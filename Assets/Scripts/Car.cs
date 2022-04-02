ff587c9f590e61705e35656405fcc29ec1098351
Tiger <thisisstreamliner@gmail.com>
2022-04-03 01:10:55 +0300

    Steering The Car

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,10 +6,20 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedGain = 0.1f;
+    [SerializeField] private float turnSpeed = 200f;
+
+    private int _steerValue;
+    
     void Update()
     {
         IncreaseSpeed();
+        transform.Rotate(0f, _steerValue * turnSpeed * Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        _steerValue = value;
     }
 
     private void IncreaseSpeed()
